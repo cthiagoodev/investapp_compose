516d854e9901bf3c497eab7d372df6e54be0799c
thiagosousadev <thiagosousadev@gmail.com>
2024-03-26 14:40:03 -0300

    alteracoes

--- investapp/src/main/java/br/com/thiagoodev/investapp/android/basics/interceptors/TokenInterceptor.kt
+++ investapp/src/main/java/br/com/thiagoodev/investapp/android/basics/interceptors/TokenInterceptor.kt
@@ -1,10 +1,29 @@
 package br.com.thiagoodev.investapp.android.basics.interceptors
 
+import okhttp3.HttpUrl
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 
 class TokenInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        TODO("Not yet implemented")
+        val request: Request = chain
+            .request()
+            .newBuilder()
+            .url(addTokenInUrl(chain.request().url()))
+            .build()
+
+        return chain.proceed(request);
+    }
+
+    private fun addTokenInUrl(url: HttpUrl): HttpUrl {
+        if(url.queryParameter("token") == null) {
+            return url
+                .newBuilder()
+                .addQueryParameter("token", "")
+                .build()
+        }
+
+        return url;
     }
 }
