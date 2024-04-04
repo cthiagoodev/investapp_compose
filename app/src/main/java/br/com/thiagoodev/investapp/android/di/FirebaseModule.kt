2ff3209d0d875c1d8b3c593b9b9df99463fe6105
cthiagoodev <thiagosousadev@gmail.com>
2024-04-04 06:34:16 -0300

    alteracoes

--- app/src/main/java/br/com/thiagoodev/investapp/android/di/FirebaseModule.kt
+++ app/src/main/java/br/com/thiagoodev/investapp/android/di/FirebaseModule.kt
@@ -5,11 +5,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ServiceComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ServiceComponent::class)
 class FirebaseModule {
     @Provides
+    @Singleton
     fun provideFirebaseAuth(): FirebaseAuth {
         return FirebaseAuth.getInstance()
     }
