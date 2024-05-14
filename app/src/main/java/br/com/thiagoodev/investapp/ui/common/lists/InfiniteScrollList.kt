ed0f68e54d36452ef1f288b835ab726f01027321
thiagosousadev <thiagosousadev@gmail.com>
2024-05-14 17:31:02 -0300

    alteracoes

--- app/src/main/java/br/com/thiagoodev/investapp/ui/common/lists/InfiniteScrollList.kt
+++ app/src/main/java/br/com/thiagoodev/investapp/ui/common/lists/InfiniteScrollList.kt
@@ -1,17 +1,54 @@
 package br.com.thiagoodev.investapp.ui.common.lists
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 
 @Composable
 fun <T> InfiniteScrollList(
     items: List<T>,
-    content: @Composable () -> Unit,
+    isLoading: Boolean = false,
+    endReached: Boolean = false,
+    loadMore: suspend () -> Unit,
     loading: @Composable () -> Unit,
-    empty: @Composable () -> Unit?,
-    error: @Composable () -> Unit?,
+    error: (@Composable () -> Unit)?,
+    content: @Composable (T) -> Unit,
 ) {
-    LazyColumn {
+    val lazyListState: LazyListState = rememberLazyListState()
 
+    LaunchedEffect(lazyListState.isScrollInProgress) {
+        val isScrolledToTheEnd: Boolean =
+            lazyListState.layoutInfo.visibleItemsInfo.lastOrNull()?.index ==
+                    lazyListState.layoutInfo.totalItemsCount - 1
+
+        if(items.isEmpty() || (isScrolledToTheEnd && isLoading && endReached)) {
+            loadMore()
+        }
+    }
+
+    LazyColumn(
+        state = lazyListState,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        items(items) {
+            content(it)
+        }
+
+        if (isLoading) {
+            items(5) {
+                loading()
+            }
+        }
+
+        if (error != null) {
+            item {
+                error()
+            }
+        }
     }
 }
--- app/src/main/java/br/com/thiagoodev/investapp/ui/quotation/components/QuotationList.kt
+++ app/src/main/java/br/com/thiagoodev/investapp/ui/quotation/components/QuotationList.kt
@@ -1,46 +1,23 @@
 package br.com.thiagoodev.investapp.ui.quotation.components
 
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import br.com.thiagoodev.investapp.domain.models.Stock
+import br.com.thiagoodev.investapp.ui.common.lists.InfiniteScrollList
 import br.com.thiagoodev.investapp.ui.quotation.QuotationViewModel
 import br.com.thiagoodev.investapp.ui.quotation.state.QuotationPaginationState
 
 @Composable
 fun QuotationList(viewModel: QuotationViewModel = hiltViewModel()) {
-    val lazyListState: LazyListState = rememberLazyListState()
-    val state: State<QuotationPaginationState?> = viewModel.uiState.observeAsState()
-    val items: List<Stock> = viewModel.uiState.value!!.items
-
-    LaunchedEffect(lazyListState.isScrollInProgress) {
-        val isScrolledToTheEnd: Boolean =
-            lazyListState.layoutInfo.visibleItemsInfo.lastOrNull()?.index ==
-                    lazyListState.layoutInfo.totalItemsCount - 1
-
-        if(state.value!!.items.isEmpty() ||
-            (isScrolledToTheEnd && !state.value!!.isLoading && !state.value!!.endReached)) {
-            viewModel.fetch()
-        }
-    }
-
     Column {
         Text(
             modifier = Modifier.padding(bottom = 10.dp),
@@ -48,35 +25,32 @@ fun QuotationList(viewModel: QuotationViewModel = hiltViewModel()) {
             style = MaterialTheme.typography.titleSmall,
         )
 
-        LazyColumn(
-            state = lazyListState,
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            items(items) {
+        val state: State<QuotationPaginationState?> = viewModel.uiState.observeAsState()
+        val items: List<Stock> = viewModel.uiState.value!!.items
+
+        InfiniteScrollList(
+            items = items,
+            loadMore = viewModel::fetch,
+            isLoading = state.value?.isLoading ?: false,
+            endReached = state.value?.endReached ?: false,
+            error = {
+                Text(
+                    modifier = Modifier.padding(bottom = 10.dp),
+                    text = state.value!!.error!!.message ?: "Ocorreu um erro",
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            },
+            loading = {
                 Box(
                     modifier = Modifier.padding(bottom = 10.dp)
-                ) { QuotationItem(it) }
-            }
-
-            if (state.value!!.isLoading) {
-                items(5) {
-                    Box(
-                        modifier = Modifier.padding(bottom = 10.dp)
-                    ) {
-                        QuotationItemShimmer()
-                    }
+                ) {
+                    QuotationItemShimmer()
                 }
             }
-
-            if (state.value!!.error != null) {
-                item {
-                    Text(
-                        modifier = Modifier.padding(bottom = 10.dp),
-                        text = state.value!!.error!!.message ?: "Ocorreu um erro",
-                        style = MaterialTheme.typography.bodySmall,
-                    )
-                }
-            }
+        ) {
+            Box(
+                modifier = Modifier.padding(bottom = 10.dp)
+            ) { QuotationItem(it) }
         }
     }
 }
